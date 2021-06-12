91323c5d010a23a36f166ba38e73c04a9d8724ad
Danilo Scipioni <daniloedu@gmail.com>
2021-06-12 11:30:33 -0300

    adicionado rota login

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+require("dotenv-safe").config();
+
 const app = express();
 
 // ==> Rotas da API:
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,11 +8,26 @@
 
 const router = require('express-promise-router')();
 const usersController = require('../controllers/users.controller');
-
+const jwt = require('jsonwebtoken');
 // ==> Definindo as rotas do CRUD - 'Product':
 
 // ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/api/products
 router.get('/users', usersController.listAllUsers);
+
+router.post('/login', (req, res, next) => {
+    //esse teste abaixo deve ser feito no seu banco de dados
+    if(req.body.user === 'danilo' && req.body.password === 'danilo89'){
+      //auth ok
+      const id = 1; //esse id viria do banco de dados
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+      return res.json({ auth: true, token: token });
+    }
+    
+    res.status(500).json({message: 'Login inválido!'});
+})
+
 
 module.exports = router;
 
