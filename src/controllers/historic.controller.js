d7e574813e8a8e9917c943732afe43e3fce4dcb9
Danilo Scipioni <daniloedu@gmail.com>
2021-06-12 10:32:59 -0300

    commit change database

--- src/controllers/historic.controller.js
+++ src/controllers/historic.controller.js
@@ -3,7 +3,7 @@ const db = require("../config/database");
 // ==> Método responsável por listar todos os 'Products':
 exports.listAllHistoric = async (req, res) => {
    
-    const response = await db.query('SELECT * FROM easyagro.tbl_historico_maquina');
+    const response = await db.query('SELECT * FROM users.tbl_users');
 
     //teste = response.rows;
     res.status(200).json(
