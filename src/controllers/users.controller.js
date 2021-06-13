f31b3d155c94916b4b19185266607673ca95730d
Danilo Scipioni <daniloedu@gmail.com>
2021-06-13 11:12:16 -0300

    finalizado configuração acesso via jwt

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,15 +2,27 @@ const db = require("../config/database");
 
 // ==> Método responsável por listar todos os 'Products':
 exports.listAllUsers = async (req, res) => {
-   
-    const response = await db.query('SELECT * FROM users.tbl_users');
 
-    //teste = response.rows;
-    res.status(200).json(
-      {
-        'data':response.rows,
-        'success':true,
-        'rowCount':response.rowCount
+  const response = await db.query('SELECT * FROM users.tbl_users');
+  //teste = response.rows;
+  res.status(200).json(
+    {
+      'data': response.rows,
+      'success': true,
+      'rowCount': response.rowCount
     });
 
-  };
+};
+
+// ==> Método responsável por listar todos os 'Products':
+exports.searchUser = async (req, res) => {
+
+  const response = await db.query(`SELECT * FROM users.tbl_users where username = '${req.user}' and password = '${req.password}'`);
+
+
+  return { data: response.rows, success: true, rowCount: response.rowCount };
+
+
+
+};
+
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -9,41 +9,46 @@
 const router = require('express-promise-router')();
 const usersController = require('../controllers/users.controller');
 const jwt = require('jsonwebtoken');
-// ==> Definindo as rotas do CRUD - 'Product':
-
-// ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/api/products
-//router.get('/users', usersController.listAllUsers);
 
 router.get('/users', verifyJWT, usersController.listAllUsers)
 
-// router.get('/users', verifyJWT, (req, res, next) => { 
-//   console.log("Retornou todos clientes!");
-//   //res.json(usersController.listAllUsers);
-//   res.json([{id:1,nome:'luiz'}]);
-// })
 
+/**
+ * Função de login
+ */
+router.post('/login', async (req, res, next) =>{
+  
+  const user = await usersController.searchUser(req.body);
 
-router.post('/login', (req, res, next) => {
-    //esse teste abaixo deve ser feito no seu banco de dados
-    if(req.body.user === 'danilo' && req.body.password === 'danilo89'){
-      //auth ok
-       const id = 1; //esse id viria do banco de dados
-       const token = jwt.sign({ id }, process.env.SECRET, {
-         expiresIn: 300 // expires in 5min
-       });
-      return res.json({ auth: true, token: token });
-}
-    
-    res.status(500).json({message: 'Login inválido!'});
+ 
+  //esse teste abaixo deve ser feito no seu banco de dados
+  //if (req.body.user === 'daniloscipioni' && req.body.password === 'danilo89') {
+    if(user.rowCount > 0){
+    //auth ok
+    const id = user.data[0].user_id; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300 // expires in 5min
+    });
+    return res.json({ auth: true, token: token });
+  }
+
+  res.status(500).json({ message: 'Login inválido!' });
 })
 
-function verifyJWT(req, res, next){
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns autenticação JWT
+ */
+function verifyJWT(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
     console.log(err)
 
     if (err) return res.sendStatus(403)
