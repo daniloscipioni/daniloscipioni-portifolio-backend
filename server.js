ec7939fa56cd30e861e85224bb4bb9c19f0ee830
Danilo Scipioni <daniloedu@gmail.com>
2021-07-08 14:01:05 -0300

    Refatoracao server.js

--- server.js
+++ server.js
@@ -12,33 +12,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Aplicação executando na porta ', port);
 });
-
-// const http = require("http");
-// const express = require("express");
-// const app = express();
-
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   //postgres://fwudoqtnyrslhf:d6eb4b8bad89212d4cb05e6ddb31cd8c1260b5e57fc650b79710eec042d4c100@ec2-18-232-254-253.compute-1.amazonaws.com:5432/d8pfiaj8njdsuv
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-
-// client.query('SELECT * FROM easyagro.tbl_categories;', (err, res) => {
-//   //if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-
-// app.get("/", function(req, res) {
-//     res.json({a:'b'});
-// });
-
-// http.createServer(app).listen(process.env.PORT || 3000, () => console.log("Servidor rodando local na porta 3000"));
