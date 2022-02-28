23b2d82e09c218eee3d29f9704a45d9b2fcef858
Danilo Scipioni <daniloedu@gmail.com>
2022-02-28 16:30:41 -0300

    update api version

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,7 +13,7 @@ router.get(['/', '/api'], (req, res) => {
   res.status(200).send({
     success: 'true',
     message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Heroku!',
-    version: '1.0.0',
+    version: '1.0.1',
     documentation: '/api-docs/',
   });
 });
