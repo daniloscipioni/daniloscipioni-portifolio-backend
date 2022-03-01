ed2131f6356761e89272bcd0ec3a1ca878120976
Danilo Scipioni <daniloedu@gmail.com>
2022-03-01 18:43:50 -0300

    create model for sequelize

---
+++ src/model/user.js
@@ -0,0 +1,44 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+//const sequelize = new Sequelize('postgres::memory:');
+const sequelize = require('../config/database-sequelize');
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  user_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nm_user: {
+    type: DataTypes.STRING(100),
+  },
+  email: {
+    type: DataTypes.STRING(100),
+  },
+  created_at: {
+    type: DataTypes.DATEONLY,
+  },
+  last_access: {
+    type: DataTypes.DATEONLY,
+  },
+  username: {
+    type: DataTypes.STRING(50),
+  },
+  password: {
+    type: DataTypes.STRING(100),
+  },
+
+}, {
+  // Other model options go here
+  tableName: 'tbl_users',
+  schema: 'users',
+  createdAt: false,
+  updatedAt: false,
+});
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
+
+module.exports = User;
